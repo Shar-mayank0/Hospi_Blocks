76c72cb127279d52ff78430c664d830010b5ecc8
adiaparmar <adiaparmar@gmail.com>
2024-10-17 21:07:54 +0530

    Connect button done

---
+++ hospi_blocks_app/src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import MetaMaskButton from "@/components/MetaMaskButton";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <MetaMaskButton />
+    </div>
+  );
+};
+
+export default page;
